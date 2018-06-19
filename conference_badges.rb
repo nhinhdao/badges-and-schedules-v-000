db28a8d24d757c753265007a93e5b75b8fdc1f88
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-19 01:00:50 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+require 'pry'
+def badge_maker(name)
+      "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+      array.map { |name|  "Hello, my name is #{name}."}
+end
+
+def assign_rooms(list)
+      newlist = []
+      list.each_with_index { |name, index| newlist.push("Hello, #{name}! You'll be assigned to room #{index + 1}!")}
+      newlist
+end
+
+def printer(list)
+      batch_badge_creator(list).each { |name| puts name}
+      assign_rooms(list).each{ |name| puts name}
+      # binding.pry
+end
